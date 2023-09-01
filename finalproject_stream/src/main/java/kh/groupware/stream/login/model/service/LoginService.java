5f669000258722c3af350449acc31a669a549438
jiYoung4868 <orinogcode@gmail.com>
2023-09-01 11:40:00 +0900

    화면설계중, 기업가입, 회원가입

--- finalproject_stream/src/main/java/kh/groupware/stream/login/model/service/LoginService.java
+++ finalproject_stream/src/main/java/kh/groupware/stream/login/model/service/LoginService.java
@@ -1,5 +1,16 @@
 package kh.groupware.stream.login.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kh.groupware.stream.login.model.vo.LoginVo;
+
+@Service
 public class LoginService {
+	
+	@Autowired
+	private LoginVo loginVo;
+	
+	
 
 }
