0d4183465f75ed59a33cc1c169115a155275dd39
JM00001 <liser1@naver.com>
2023-09-20 11:56:21 +0900

    하위 업무 추가를 위한 ddl 수정2
    컬럼명 변경
    + 사용할 sql문 확인중

--- SQL/jm_sql.sql
+++ SQL/jm_sql.sql
@@ -26,3 +26,15 @@ select  pname, pno, cnt, ttitle, userid, tstatus, tdate, tstartdate, tenddate
 select pname, pno, ttitle, userid, tstatus, tdate, tstartdate, tenddate
     from project join task using(pno, userid)
     ;
+
+--원본글, 답글 확인하기 
+--bref=tno : 원본글 bref <> tno : 답글
+select bref from task where tno =1;
+
+--brefstep = 1~N : 하위 업무들의 순서
+select brestep+1 from task where tno =1;
+
+--brelevel=0 : 원본글, =1 : 하위 업무
+select brelevel+1 from task where tno = 1;
+
+
