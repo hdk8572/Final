e4e251b16d064b74c9685494b71b0d10246dba47
JM00001 <liser1@naver.com>
2023-09-27 10:13:40 +0900

    시큐리티 확인

--- stream_final_new/src/main/java/kh/groupware/stream/project/controller/ProjectController.java
+++ stream_final_new/src/main/java/kh/groupware/stream/project/controller/ProjectController.java
@@ -1,29 +1,20 @@
 
 package kh.groupware.stream.project.controller;
 
-import java.security.Principal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.context.request.RequestAttributes;
 
-import com.google.gson.Gson;
 
 import kh.groupware.stream.project.model.service.ProjectService;
-import kh.groupware.stream.project.model.service.ProjectServiceImpl;
 import kh.groupware.stream.project.model.vo.ProjectVo;
-import kh.groupware.stream.ptask.model.service.PtaskService;
 
 @Controller
 public class ProjectController {
