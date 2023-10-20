563b5ec2a2ee74e06a979e2f0ee9b13130344dd6
alstjd0930 <alstjd0930@naver.com>
2023-10-20 09:15:29 +0900

    stompchat다시

--- stream_final_new/src/main/java/kh/groupware/stream/chat/controller/StompChatController.java
+++ stream_final_new/src/main/java/kh/groupware/stream/chat/controller/StompChatController.java
@@ -25,7 +25,6 @@ public class StompChatController {
     @MessageMapping(value = "/chat/enter")//서버에서 보내주는 컨트롤러
     public void enter(ChatMessageVo message,Principal principal) {
     	String userId = principal.getName();
-<<<<<<< HEAD
     	message.setMessage(chatDao.findWriter(userId).getMname() +"님이 채팅방에 참여하였습니다.");
     	message.setMname(chatDao.findWriter(userId).getMname());
         template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
@@ -45,18 +44,11 @@ public class StompChatController {
     	chatDao.MessageInsert(message);
     	message.setMname(userId);
         template.convertAndSend("/sub/arm/" + message.getRoomId(), message);      
-=======
-    	message.setMessage(chatDao.findWriter(userId).getmName() +"님이 채팅방에 참여하였습니다.");
-    	message.setmName(chatDao.findWriter(userId).getmName());
+    	message.setMessage(chatDao.findWriter(userId).getMname() +"님이 채팅방에 참여하였습니다.");
+    	message.setMname(chatDao.findWriter(userId).getMname());
         template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
-        System.out.println(chatDao.findWriter(userId).getmName()+"username확인");
+        System.out.println(chatDao.findWriter(userId).getMname()+"username확인");
         System.out.println(message);
     }
 
-    @MessageMapping(value = "/chat/message")//서버에 보내는 컨트롤러
-    public void message(ChatMessageVo message){
-    	chatDao.MessageInsert(message);
-        template.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
->>>>>>> 072d9c8fe7d0e80d9a1b7ec6600a27fe032ee880
-    }
 }
