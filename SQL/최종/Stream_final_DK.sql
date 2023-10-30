c104135ed77e7317c97fc3734e387ceda3dc23a9
JM00001 <liser1@naver.com>
2023-10-30 09:35:21 +0900

    DML task bref 관련 수정.
    insert에 bref=tno 되도록 수정

--- SQL/최종/Stream_final_DK.sql
+++ SQL/최종/Stream_final_DK.sql
@@ -34,69 +34,69 @@ INTO MEMBER_PROJECT(PNO, USERID) values (project_sequence.NEXTVAL, 'streamjj1@na
 -- 공공데이터 융합 자바개발자 양성과정A23 - 업무 목록
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '1', 'streamjj1@naver.co.kr', 'streamjj1@naver.co.kr', 
-'1_프로그래밍 기초', '1_프로그래밍 기초', '진행', '2023-10-29', '2023-10-31', '1', '0', '0')
+'1_프로그래밍 기초', '1_프로그래밍 기초', '진행', '2023-10-29', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '1', 'streamjj1@naver.co.kr', 'streamjj1@naver.co.kr', 
-'2_변수', '2_변수', '진행', '2023-10-29', '2023-10-30', '1', '0', '0')
+'2_변수', '2_변수', '진행', '2023-10-29', '2023-10-30', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '1', 'streamjj3@naver.co.kr', 'streamjj3@naver.co.kr', 
-'3_연산자', '3_연산자', '진행', '2023-10-02', '2023-10-31', '1', '0', '0')
+'3_연산자', '3_연산자', '진행', '2023-10-02', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '1', 'streamjj2@naver.co.kr', 'streamjj2@naver.co.kr', 
-'4_제어문', '4_제어문', '진행', '2023-10-04', '2023-10-31', '1', '0', '0')
+'4_제어문', '4_제어문', '진행', '2023-10-04', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 -- 포트폴리오 제작 - 업무 목록
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '2', 'streamjj1@naver.co.kr', 'streamjj1@naver.co.kr', 
-'템플릿 선정', '템플릿을 선정해주세요', '진행', '2023-10-01', '2023-10-31', '1', '0', '0')
+'템플릿 선정', '템플릿을 선정해주세요', '진행', '2023-10-01', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '2', 'streamjj2@naver.co.kr', 'streamjj2@naver.co.kr', 
-'유스케이스 작성', '유스케이스 작성해주세요', '진행', '2023-10-02', '2023-10-31', '1', '0', '0')
+'유스케이스 작성', '유스케이스 작성해주세요', '진행', '2023-10-02', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '2', 'streamjj3@naver.co.kr', 'streamjj3@naver.co.kr', 
-'클래스다이어그램 작성', '클래스 다이어그램 작성해주세요', '진행', '2023-10-03', '2023-10-31', '1', '0', '0')
+'클래스다이어그램 작성', '클래스 다이어그램 작성해주세요', '진행', '2023-10-03', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '2', 'streamjj@naver.co.kr', 'streamjj@naver.co.kr', 
-'화면설계서 작성', '화면설계서 작성해주세요', '진행', '2023-10-03', '2023-10-31', '1', '0', '0')
+'화면설계서 작성', '화면설계서 작성해주세요', '진행', '2023-10-03', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 --프로젝트 깃허브주소 - 업무목록
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '3', 'streamjj@naver.co.kr', 'streamjj@naver.co.kr', 
-'회원가입 요청드립니다.', '신청했습니다!', '미진행', '2023-10-04', '2023-10-31', '1', '0', '0')
+'회원가입 요청드립니다.', '신청했습니다!', '미진행', '2023-10-04', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '3', 'streamjj1@naver.co.kr', 'streamjj1@naver.co.kr', 
-'회원가입 요청이요.', '확인 부탁드립니다.', '미진행', '2023-10-05', '2023-10-31', '1', '0', '0')
+'회원가입 요청이요.', '확인 부탁드립니다.', '미진행', '2023-10-05', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '3', 'streamjj2@naver.co.kr', 'streamjj2@naver.co.kr', 
-'회원가입 요청했습니다.', '확인 부탁드립니다.', '미진행', '2023-10-04', '2023-10-31', '1', '0', '0')
+'회원가입 요청했습니다.', '확인 부탁드립니다.', '미진행', '2023-10-04', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '3', 'streamjj3@naver.co.kr', 'streamjj3@naver.co.kr', 
-'회원가입 받아주세요', '신청했습니다!', '미진행', '2023-10-04', '2023-10-31', '1', '0', '0')
+'회원가입 받아주세요', '신청했습니다!', '미진행', '2023-10-04', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 INSERT INTO TASK (TNO, PNO, USERID, TMEMBER, TTITLE, TCONTENT, TSTATUS, tstartdate, tenddate, 
 bref, brelevel, brestep) VALUES (task_sequence.NEXTVAL, '3', 'streamjj4@naver.co.kr', 'streamjj4@naver.co.kr', 
-'회원가입 요청보냈는데... ', '확인 부탁드립니다.', '미진행', '2023-10-03', '2023-10-31', '1', '0', '0')
+'회원가입 요청보냈는데... ', '확인 부탁드립니다.', '미진행', '2023-10-03', '2023-10-31', task_sequence.NEXTVAL, '0', '0')
 ;
 
 -- 프로젝트 깃허브주소 - 댓글
